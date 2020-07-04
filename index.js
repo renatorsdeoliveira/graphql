e88fc475e566242ace14ab30418576769da27aa8
Renato_O <renatorsdeoliveira>
2020-07-03 22:28:15 -0300

    Criado a primeira query e resolver

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("teste")
+const { ApolloServer, gql } = require ('apollo-server')
+
+const typeDefs = gql `
+    #pontos de entrada
+    type Query {
+        ola: String
+    }
+
+`
+
+const resolvers = {
+    Query: {
+        ola() {
+            return "retornando testo"
+        }
+    }
+
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Execultando em ${url}`)
+})
