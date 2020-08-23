6add5287bf02d4fa24fd3d8d047e3efde1413504
Renato_O <renatorsdeoliveira@gmail.com>
2020-08-23 08:41:18 -0300

    Consulta em Array personalizado (Tipo Usuario)

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { ApolloServer, gql } = require ('apollo-server')
 
+const usuarios = [{
+    id: 1,
+    nome: 'João',
+    email: 'joao@gmail.com',
+    idade: 24
+}, {
+    id: 2,
+    nome: 'Maria',
+    email: 'maria@gmail.com',
+    idade: 29
+}, {
+    id: 3,
+    nome: 'Joana',
+    email: 'joana@gmail.com',
+    idade: 20
+}]
+
 const typeDefs = gql `
     scalar Date
     type Produto{
@@ -10,8 +27,9 @@ const typeDefs = gql `
     }
     
     type Usuario {
-        id: ID,
+        id: ID
         nome: String
+        idade: Int
         email: String
         telefone: Int
         salario: Float
@@ -26,6 +44,7 @@ const typeDefs = gql `
         usauario: Usuario
         produtoEmDesaque: Produto
         numeroMegaSena: [Int!]!
+        usuarios: [Usuario]
     }
 
 
@@ -80,12 +99,15 @@ const resolvers = {
             return Array(6).fill(0)
             .map(n => parseInt(Math.random() * 60 + 1))
             .sort(crescente)
+        },
+        usuarios(){
+            return usuarios
         }
     }
 
 }
 
-const server = new ApolloServer({
+const server = new ApolloServer({   
     typeDefs,
     resolvers
 })
